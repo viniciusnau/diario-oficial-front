6dec003edf649df55cfa9c42596422135df45ae8
Guilherme Pereira <guilhermepereira@defensoria.sc.gov.br>
2023-08-14 17:14:14 -0300

    Feat: responsiveness of footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -12,29 +12,25 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.topics}>
-        <div className={styles.content}>
-          <p className={styles.description}>
-            <strong>Objetivo:</strong> Atualizar a comunidade de forma prática
-            sobre as notícias que acontecem na DPE-SC
-            <br />
-            Publicações nos dias úteis: 00h00
-            <br />
-          </p>
-        </div>
+        <p className={styles.description}>
+          <strong>Objetivo:</strong> Atualizar a comunidade de forma prática
+          sobre as notícias que acontecem na DPE-SC
+          <br />
+          Publicações nos dias úteis: 00h00
+          <br />
+        </p>
         <p className={styles.copyright}>
           &copy; Desenvolvido pela Gerência de Tecnologia da Informação da
           Defensoria Pública do Estado de Santa Catarina. Todos os direitos
           reservados {new Date().getFullYear()}.
         </p>
-        <div className={styles.content}>
-          <p className={styles.description}>
-            <strong>Endereço:</strong> Av. Rio Branco, nº 919, Ed. Centro
-            Executivo Rio Branco, Centro, Florianópolis/SC, 88015-205
-            <br />
-            <strong>Contato:</strong> (48) 3665-6370 / (48) 3665-6589 / (48)
-            3665-6654
-          </p>
-        </div>
+        <p className={styles.description}>
+          <strong>Endereço:</strong> Av. Rio Branco, nº 919, Ed. Centro
+          Executivo Rio Branco, Centro, Florianópolis/SC, 88015-205
+          <br />
+          <strong>Contato:</strong> (48) 3665-6370 / (48) 3665-6589 / (48)
+          3665-6654
+        </p>
       </div>
     </footer>
   );
